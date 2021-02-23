d3bacc8d91fcf6295e0b11410d5a493af2f8c489
unknown <rotsen.adalid@gmail.com>
2021-02-23 13:55:47 -0400

    logo neta

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -7,7 +7,7 @@
   <meta property="og:url"        content="http://yosolidario.com" />
   <meta property="og:type"       content="article" />
   <meta property="og:title"      content="YoSolidario - {{__('The crowdfunding platform')}}" />
-  <meta property="og:description"  content="{{__('YoSolidario: The most trusted online fundraising platform. Start a crowdfunding campaign. ✓ Read our guarantee!')}}" />
+  <meta property="og:description"  content="{{__('The most trusted online fundraising platform. Start a crowdfunding campaign. ✓ Read our guarantee!')}}" />
   <meta property="og:image"      content="{{asset('images/logo-ceo.png')}}" />
   <meta property="fb:app_id" content="738141669970459" />
 
@@ -16,7 +16,7 @@
   <meta name="twitter:site" content="@yosolidariocom">
   <meta name="twitter:title" content="YoSolidario - {{__('The crowdfunding platform')}}}">
   <meta name="twitter:creator" content="@yosolidariocom">
-  <meta name="twitter:description" content="{{__('YoSolidario: The most trusted online fundraising platform. Start a crowdfunding campaign. ✓ Read our guarantee!')}}">
+  <meta name="twitter:description" content="{{__('The most trusted online fundraising platform. Start a crowdfunding campaign. ✓ Read our guarantee!')}}">
   <meta name="twitter:image" content="{{asset('images/logo-ceo.png')}}">
   
 </x-slot>
