1658fc648d83ba69f089e6d0dd7ee82b6d0cd51d
unknown <rotsen.adalid@gmail.com>
2021-02-25 14:43:08 -0400

    meta campaign

--- resources/views/livewire/campaigns/published/show-published.blade.php
+++ resources/views/livewire/campaigns/published/show-published.blade.php
@@ -4,13 +4,25 @@
 <x-slot  name="seo">
     
         <!-- facebook -->
-        <meta property="og:url"        content="http://www.yosolidario.com/f/{{$campaign->slug}}" />
+        <meta property="og:site_name" content="NOMBRE_DEL_SITIO">
+        <meta property="og:url" content="http://www.yosolidario.com/f/{{$campaign->slug}}">
         <meta property="og:type" content="video">
-        <meta property="og:title"      content="{{$campaign->title}}" />
-        <meta property="og:description"  content="{{$campaign->extract}}" />
-        <meta property="og:image"      content="https://yosolidario.com{{$campaign->image->url}}" />
-        <meta property="og:video:url"      content="{{$campaign->video->url}}" />
-        <meta property="fb:app_id" content="738141669970459" />
+        <meta property="og:title" content="TÍTULO_DEL_VIDEO">
+        <meta property="og:description" content="DESCRIPCIÓN_DEL_VIDEO">
+        <meta property="og:image" content="https://yosolidario.com{{$campaign->image->url}}">
+        <meta property="og:image:type" content="image/jpg">
+        <meta property="og:image:width" content="1280">
+        <meta property="og:image:height" content="720">
+        <meta property="og:video:url" content="https://player.vimeo.com/video/491927546?autoplay=1">
+        <meta property="og:video:secure_url" content="https://player.vimeo.com/video/491927546?autoplay=1">
+        <meta property="og:video:type" content="text/html">
+        <meta property="og:video:width" content="1280">
+        <meta property="og:video:height" content="720">
+        <meta property="og:video:url" content="https://vimeo.com/moogaloop.swf?clip_id=491927546&amp;autoplay=1">
+        <meta property="og:video:secure_url" content="https://vimeo.com/moogaloop.swf?clip_id=491927546&amp;autoplay=1">
+        <meta property="og:video:type" content="application/x-shockwave-flash">
+        <meta property="og:video:width" content="1280">
+        <meta property="og:video:height" content="720">
 
         <!-- twitter -->
         <meta name="twitter:card" content="summary_large_image">
