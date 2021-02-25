9f8402d1f95d45260a43b91f8370c8c3509aec2f
unknown <rotsen.adalid@gmail.com>
2021-02-25 14:44:43 -0400

    meta campaign

--- resources/views/livewire/campaigns/published/show-published.blade.php
+++ resources/views/livewire/campaigns/published/show-published.blade.php
@@ -7,8 +7,8 @@
         <meta property="og:site_name" content="NOMBRE_DEL_SITIO">
         <meta property="og:url" content="http://www.yosolidario.com/f/{{$campaign->slug}}">
         <meta property="og:type" content="video">
-        <meta property="og:title" content="TÍTULO_DEL_VIDEO">
-        <meta property="og:description" content="DESCRIPCIÓN_DEL_VIDEO">
+        <meta property="og:title" content="{{$campaign->title}}">
+        <meta property="og:description" content="{{$campaign->extract}}">
         <meta property="og:image" content="https://yosolidario.com{{$campaign->image->url}}">
         <meta property="og:image:type" content="image/jpg">
         <meta property="og:image:width" content="1280">
@@ -23,7 +23,7 @@
         <meta property="og:video:type" content="application/x-shockwave-flash">
         <meta property="og:video:width" content="1280">
         <meta property="og:video:height" content="720">
-
+        <meta property="fb:app_id" content="738141669970459" />
         <!-- twitter -->
         <meta name="twitter:card" content="summary_large_image">
         <meta name="twitter:site" content="@yosolidariocom">
